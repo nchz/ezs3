b5c27bb66d9364ecb431570fe8cad94c1bb75b7a
nchz <nchngz@gmail.com>
2020-04-07 19:55:27 -0300

    setup and requirements.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="ezs3",
+    version="0.0.1",
+    author="nchz",
+    description="S3 wrapper built on top of `boto3`.",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=requirements,
+)
